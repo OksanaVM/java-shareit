7abc6287acbcaa0b892fd9797bcc774b4963de13
Oksana_Makarova <79106047+OksanaVM@users.noreply.github.com>
2023-04-07 19:01:54 +0300

    cod

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
